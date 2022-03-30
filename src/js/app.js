492621f08d38593059ca8760c5399b1560b41156
Kaptikov <igor.zabirow02@mail.ru>
2022-03-30 10:39:23 +0500

    SectionServices

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,13 @@ flsFunctions.isWebp();
 
 import Swiper, { Navigation, Pagination } from 'swiper';
 
-const swiper = new Swiper('.swiper', {
-  // configure Swiper to use modules
-  modules: [Navigation, Pagination]
-});
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper(".Swiper", {
+        scrollbar: {
+          el: ".swiper-scrollbar",
+          hide: true,
+        },
+      });
