324e846a2e932ca9cd027982da6b0d56be87d76e
Kaptikov <igor.zabirow02@mail.ru>
2022-04-11 16:25:31 +0500

    header-burger

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,41 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
-import Swiper, { Navigation, Pagination } from 'swiper';
+const burger = document?.querySelector('[data-burger]');
+const menu = document?.querySelector('[data-menu]');
+const menuItems = menu?.querySelectorAll('a')
+const body = document.body;
+const header = document?.querySelector('.header__block');
+const headerHeight = header.offsetHeight;
+document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+burger?.addEventListener('click', () => {
+  body.classList.toggle('stop--scroll');
+  burger?.classList.toggle('burger--active');
+  menu?.classList.toggle('header__menu--visible');
+});
 
-const swiper = new Swiper(".Swiper", {
-        scrollbar: {
-          el: ".swiper-scrollbar",
-          hide: true,
-        },
-      });
+menuItems.forEach(el => {
+  el.addEventListener('click', () => {
+    body.classList.remove('stop--scroll');
+    burger?.classList.remove('burger--active');
+    menu?.classList.remove('header__menu--visible');
+  })
+});
+
+// import Swiper, { Navigation, Pagination } from 'swiper';
+
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+
+
+
+
+// const swiper = new Swiper(".Swiper", {
+//         scrollbar: {
+//           el: ".swiper-scrollbar",
+//           hide: true,
+//         },
+//       });
